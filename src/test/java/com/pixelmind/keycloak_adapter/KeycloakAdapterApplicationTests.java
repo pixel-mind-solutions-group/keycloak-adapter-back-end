c70f94f7f34175de34e9cdc6c3e64b42b94f34d4
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2026-03-08 23:50:35 +0530

    initial project

---
+++ src/test/java/com/pixelmind/keycloak_adapter/KeycloakAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelmind.keycloak_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
